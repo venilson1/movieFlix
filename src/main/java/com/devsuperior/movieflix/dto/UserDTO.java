a5bc5081034408243dd177a4260b09a5c823073e
venilson1 <venilson2009@hotmail.com>
2022-03-22 11:15:09 -0300

    dto Role and User

---
+++ src/main/java/com/devsuperior/movieflix/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.devsuperior.movieflix.entities.User;
+
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String email;
+	Set<RoleDTO> roles = new HashSet<>();
+
+	public UserDTO() {
+	}
+
+	public UserDTO(Long id, String email) {
+		this.id = id;
+		this.email = email;
+	}
+
+	public UserDTO(User entity) {
+		id = entity.getId();
+		email = entity.getEmail();
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<RoleDTO> getRoles() {
+		return roles;
+	}
+}
