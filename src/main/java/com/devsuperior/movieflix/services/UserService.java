4d737bbae03a15ac20abd28978b19bfcb3cf1e3c
venilson1 <venilson2009@hotmail.com>
2022-03-22 14:03:33 -0300

    user service

--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -2,7 +2,12 @@ package com.devsuperior.movieflix.services;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,8 +17,10 @@ import com.devsuperior.movieflix.repositories.UserRepository;
 import com.devsuperior.movieflix.services.Exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
-	
+public class UserService implements UserDetailsService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
 	@Autowired
 	private UserRepository repository;
 
@@ -22,6 +29,17 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new UserDTO(entity);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("User found: " + username);
+		return user;
 	}
 
 }
